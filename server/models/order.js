e57969dd72154d44fa1f6863555d324ae5587569
Jesper Bruun Hansen <jesperbbhansen@gmail.com>
2017-10-28 13:24:45 +0200

    add custom endpoint

--- server/models/order.js
+++ server/models/order.js
@@ -1,5 +1,40 @@
 'use strict';
 
-module.exports = function(Order) {
+const Promise = require("bluebird");
+
+module.exports = function (Order) {
+
+  Order.getFullInfo = (userId ,filter, options, next) => {
+    Order.find({
+      "where": {
+        "createdById": userId
+      }
+    }).then(orders => {
+      Promise.each(orders, (order) => {
+        return Promise.each(order.__data.orderItems, item => {
+          var foo =1;
+          return Order.app.models.Book.findById(item.bookId).then(book => item.bookInfo = book);
+        })
+      }).then(() => {
+        next(null, orders);
+      });
+    }).catch(next);
+  };
+
+  Order.remoteMethod(
+    'getFullInfo',
+    {
+      description: 'Flags this Moment instance',
+      accepts: [
+        {arg: 'id', type: 'string', description: "User id", required: true},
+        {arg: 'filter', type: 'object', http: {source: 'query'}},
+        {arg: "options", type: "object", http: "optionsFromRequest"}
+      ],
+      returns: {
+        arg: 'Order', type: 'Order', root: true
+      },
+      http: {path: '/:id/allorders', verb: 'get'}
+    }
+  );
 
 };
